b2078fb9dd3fe89324b3f145605347c4b1929570
kalpanabolli11@gmail.com <kalpanabolli11@gmail.com>
2023-08-01 18:52:03 +0530

    first commit

---
+++ Subject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProject_4
+
+{
+    public class Subject
+    {
+        public string Name { get; set; }
+        public string SubjectCode { get; set; }
+        public Teacher Teacher { get; set; }
+    }
+}
---
+++ TeacherWithSubjects.cs
@@ -0,0 +1,21 @@
+﻿using PracticeProject_4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProject_4
+{
+    public class TeacherWithSubjects
+    {
+        public Teacher Teacher { get; set; }
+        public List<Subject> Subjects { get; set; }
+
+        public TeacherWithSubjects()
+        {
+            Subjects = new List<Subject>();
+        }
+    }
+
+}
